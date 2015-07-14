54eb85831ecb156c002f334af42d7b1f7b28e425
root <root@localhost>
2015-07-14 11:48:01 -0700

    UPDATED TO BOF7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_zeroltezt-userdebug
-add_lunch_combo cm_zeroltezt-eng
+add_lunch_combo omni_zeroltespr-userdebug
+add_lunch_combo omni_zeroltespr-eng
